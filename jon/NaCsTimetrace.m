245ecf88d18b3d2fe1d0858a681433aac14078c0
Tweezer 2 <niharvardlab@gmail.com>
2018-01-18 16:38:51 -0500

    Scanning modulation depth and measuring atom temperatures

--- jon/NaCsTimetrace.m
+++ jon/NaCsTimetrace.m
@@ -3,8 +3,9 @@
 %    H:\MergeTimeTraces.CSV
 
 %% Initialize variables.
-%% Initialize variables.
-filename = 'N:\NaCsLab\Data\20180117\TimeTraces\T0001ALL.CSV';
+%filename = 'N:\NaCsLab\Data\20180117\TimeTraces\T0001ALL.CSV';
+filename = 'G:\T0000CH2.CSV'
+
 delimiter = ',';
 startRow = 16;
 formatSpec = '%f%f%f%f%f%[^\n\r]';
@@ -15,15 +16,70 @@ fclose(fileID);
 
 t = dataArray{1};
 Ch1 = dataArray{2};
-Ch2 = dataArray{4};
+%Ch2 = dataArray{4};
 length(t)
 dt = t(2) - t(1);
 
 figure(1); clf; 
 plot(t*1e3, Ch1);
-hold on; 
+%hold on; 
 %plot(t*1e3, 0.9*Ch2/max(Ch2));
-title(['Na (red) and Cs (blue) time traces,  dt=' num2str(dt*1e3) 'ms' ]);
+title(['AOD linearRamp,  dt=' num2str(dt*1e3) 'ms' ]);
+xlabel('Time (ms)');
+ylabel('V');
+%ylim([0, 1.05]);
+
+%% 2
+%filename = 'N:\NaCsLab\Data\20180117\TimeTraces\T0001ALL.CSV';
+filename = 'G:\T0001CH2.CSV'
+
+delimiter = ',';
+startRow = 16;
+formatSpec = '%f%f%f%f%f%[^\n\r]';
+fileID = fopen(filename,'r');
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false);
+fclose(fileID);
+
+t = dataArray{1};
+Ch1 = dataArray{2};
+%Ch2 = dataArray{4};
+length(t)
+dt = t(2) - t(1);
+
+figure(1); clf; 
+plot(t*1e3, Ch1);
+%hold on; 
+%plot(t*1e3, 0.9*Ch2/max(Ch2));
+title(['AOD trapMove,  dt=' num2str(dt*1e3) 'ms' ]);
+xlabel('Time (ms)');
+ylabel('V');
+%ylim([0, 1.05]);
+
+
+%% 3
+%filename = 'N:\NaCsLab\Data\20180117\TimeTraces\T0001ALL.CSV';
+filename = 'G:\T0002CH2.CSV'
+
+delimiter = ',';
+startRow = 16;
+formatSpec = '%f%f%f%f%f%[^\n\r]';
+fileID = fopen(filename,'r');
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false);
+fclose(fileID);
+
+t = dataArray{1};
+Ch1 = dataArray{2};
+%Ch2 = dataArray{4};
+length(t)
+dt = t(2) - t(1);
+
+figure(1); clf; 
+plot(t*1e3, Ch1);
+%hold on; 
+%plot(t*1e3, 0.9*Ch2/max(Ch2));
+title(['AOD trapMove, DC  dt=' num2str(dt*1e3) 'ms' ]);
 xlabel('Time (ms)');
 ylabel('V');
 %ylim([0, 1.05]);
